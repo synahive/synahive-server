344fde73d64caa7e48fab37dc3c9b5d37cfc2051
Marius Meissner <admin@synahive.com>
2016-04-16 22:52:41 +0000

    Entity Additions Authorization Endpoint

--- src/main/scala/synahive/restapi/models/SubscriptionEntity.scala
+++ src/main/scala/synahive/restapi/models/SubscriptionEntity.scala
@@ -2,7 +2,7 @@ package synahive.restapi.models
 
 import org.mindrot.jbcrypt.BCrypt
 
-case class SubscriptionEntity(id: Option[Long] = None, startDate: Date, endDate: Date, ) {
+case class SubscriptionEntity(id: Option[Long] = None, startDate: Date, endDate: Date) {
   require(!startDate.isEmpty, "start_date.empty")
   require(!endDate.isEmpty, "end_date.empty")
 }
